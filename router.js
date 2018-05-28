34291c2b31cb4c5205f41ea027face64d9c25ccc
cainiaozone <cainiaozone@163.com>
2018-05-28 08:48:58 +0800

    路由更新

--- router.js
+++ router.js
@@ -7,17 +7,11 @@ module.exports = (app) => {
   /**
    * 模板文件
    */
-<<<<<<< HEAD
-  router.get('/', templateController.template)
-    .get('/template', templateController.template)
-=======
   router.get('/template', templateController.template)
 
     .get('/', templateController.template)
     .get('/scrollnav', pluginsController.scrollNav)
     .get('/yocarousel', pluginsController.yoCarousel)
-
->>>>>>> b7c5d51c1b208293aa24a11a70baeddc953ea2ef
 
   app.use(router.routes())
     .use(router.allowedMethods())
